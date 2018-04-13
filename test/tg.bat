8450a154a06c9ed424aa76599f8c1cf6bdfd4efd
Martin Moene <martin.moene@gmail.com>
2018-04-13 23:45:45 +0200

    Add warning to enforce strict aliasing under C++98

--- test/tg.bat
+++ test/tg.bat
@@ -1,6 +1,6 @@
 @setlocal
 @set std=%1
 @if "%std%"=="" set std=c++11
-g++ -std=%std% -O2 -Wall -Wextra -Wno-unused-parameter -o byte-lite.t.exe -I../include/nonstd byte-lite.t.cpp byte.t.cpp && byte-lite.t.exe
+g++ -std=%std% -O2 -Wall -Wextra -fstrict-aliasing -Wstrict-aliasing=2 -Wno-unused-parameter -o byte-lite.t.exe -I../include/nonstd byte-lite.t.cpp byte.t.cpp && byte-lite.t.exe
 @endlocal
 
